aded5d8c554bdebf3b76f3198a6608d3a13e2232
Ekaterina Nikonova <enikonovad@gmail.com>
2023-03-17 15:03:29 +1100

    feat: Push changes to ssp

--- scripts/automation/update_ssp.sh
+++ scripts/automation/update_ssp.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
 export COMMIT_TITLE="chore: Component definitions automatic update."
-export COMMIT_BODY="Sync components with OCP4-cd repo"
-git clone https://$GITHUB_TOKEN@github.ibm.com/compliance-trestle-testing/ocp4-ssp
-cd ocp4-ssp
-git checkout -b "cd_autoupdate_$TRAVIS_BUILD_NUMBER"
-cp -r ../component-definitions .
+export COMMIT_BODY="Sync profiles with ocp-oscal-cd repo"
+git config --global user.email "automation@example.com"
+git config --global user.name "AutomationBot" 
+cd ocp-oscal-cd
+git checkout -b "cd_autoupdate_$GITHUB_RUN_ID"
+cp -r ../catalogs .
+cp -r ../profiles . 
 if [ -z "$(git status --porcelain)" ]; then 
   echo "Nothing to commit" 
 else 
   git diff
-  git add component-definitions 
-  git commit -m "$COMMIT_TITLE"
-  git push -u origin "cd_autoupdate_$TRAVIS_BUILD_NUMBER"
-  echo $COMMIT_BODY
-  curl -u Ekaterina-Nikonova:$GITHUB_TOKEN -X POST -H "Accept: application/vnd.github.v3+json"  https://api.github.ibm.com/repos/compliance-trestle-testing/ocp4-ssp/pulls -d '{"head":"'"cd_autoupdate_$TRAVIS_BUILD_NUMBER"'","base":"develop","body":"'"$COMMIT_BODY"'","title":"'"$COMMIT_TITLE"'"}'
-
+  git add component-definitions
+  if [ -z "$(git status --untracked-files=no --porcelain)" ]; then 
+     echo "Nothing to commit" 
+  else
+     git commit -m "$COMMIT_TITLE"
+     remote=https://$GIT_TOKEN@github.com/ComplianceAsCode/ocp-oscal-ssp
+     git push -u "$remote" "cd_autoupdate_$GITHUB_RUN_ID"
+     echo $COMMIT_BODY
+     gh pr create -t "$COMMIT_TITLE" -b "$COMMIT_BODY" -B "develop" -H "cd_autoupdate_$GITHUB_RUN_ID" 
+  fi
 fi
 
